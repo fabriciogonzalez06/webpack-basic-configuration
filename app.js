ff87597815ceb9033d5d85062da864c17f9e90fe
Fabricio gonzalez <angelfabriciogonzalez@gmail.com>
2020-06-07 14:33:08 -0600

    first commit

---
+++ app.js
@@ -0,0 +1,6 @@
+const clonar = document.querySelector('#clonar');
+const elemento = document.createElement('div')
+
+clonar.addEventListener('click', ($event) => {
+    window.resizeTo(50, 50);
+});
