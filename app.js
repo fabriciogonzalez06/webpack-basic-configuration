3bcffa9ae8aaef86cfa4ec667db44353543a9bbd
Fabricio gonzalez <angelfabriciogonzalez@gmail.com>
2020-06-07 14:37:35 -0600

    fixed nodemodules

--- app.js
+++
@@ -1,6 +0,0 @@
-const clonar = document.querySelector('#clonar');
-const elemento = document.createElement('div')
-
-clonar.addEventListener('click', ($event) => {
-    window.resizeTo(50, 50);
-});
